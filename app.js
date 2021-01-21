465cc0227217cb3a7e5f94dedf7c68ec539fd45f
Danica Tiegs <clear>
2021-01-20 18:32:18 -0800

    added functions

--- app.js
+++ app.js
@@ -1,141 +1,106 @@
-// unpack function - index 1 name, 2 metadata, 3 sample?
-function unpack(rows, index) {
-  return rows.map(function(row) {
-    return row[index];
-  });
-}
-
 // Add event listener for submit button
-d3.select("#submit").on("click", handleSubmit);
+//d3.select("#submit").on("click", handleSubmit);
 
-// Submit Button handler
-function handleSubmit() {
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
+// // Submit Button handler
+// function handleSubmit() {
+//   // Prevent the page from refreshing
+//   d3.event.preventDefault();
 
-  // Select the input value from the form
-  // Q: What does `.node()` do?
-  var names = d3.select("#WHATGOESHERE?").node().value;
-  console.log(names);
+//   // Select the input value from the form
+//   // Q: What does `.node()` do?
+//   var names = d3.select("#WHATGOESHERE?").node().value;
+//   console.log(names);
 
-  // clear the input value
-  d3.select("#stockInput").node().value = "";
+//   // clear the input value
+//   d3.select("#selDataset").node().value = "";
 
-  // Update the Dashboard!
-  updateDash(stock);
-}
+//   // Update the Dashboard!
+//   updateDash(names);
+//}
 
 // Use d3.json() to fetch data from JSON file
 // Incoming data is internally referred to as incomingData
 d3.json("data/samples.json").then((importedData) => {
-  console.log(importedData),
-  var data = importedData;
-};
-
-// Demographic info (id, ethnicity, gender, age, location, bbtype, wfreq)
-// Update Demographic info with Test Subject ID No Dropdown Update
-
-// //compare function to display top 10
-
-data.sort(function(firstNum, secondNum) {
-   return firstNum - secondNum;
- })
- console.log(numArray);
-
-// error function
-function errorFunc(err) {
-  console.log(err);
-}
-
-// success function
-function successSort(data) {
-  // data.results is the array of characters
-  data.results.sort(function(firstChar, secondChar) {
-    return firstChar.height - secondChar.height;
+  console.log(importedData)
+  var dropDown = d3.selectAll("#selDataset")
+  importedData.names.forEach((id)=>{
+    //console.log(id)
+    dropDown.append("option").text(id).property("value",id)
   })
-  console.log(data.results);
+  updatePlotly(importedData.names[0])
+  var data = importedData;
+});
+
+// This function is called when a dropdown menu item is selected.
+function optionChanged(id){
+  console.log(id)
+  updatePlotly(id)
 }
 
-d3.json(url).then(successSort, errorFunc);
-
-
-// // 1.Initialize the page with a default plot
-
-// // Call updatePlotly() when a change takes place to the DOM.
-
-d3.selectAll("#selDataset").on("change", updatePlotly);
-
-// // This function is called when a dropdown menu item is selected.
-
-function updatePlotly() {
+function updatePlotly(id) {
   // Use D3 to select the dropdown menu
   var dropdownMenu = d3.select("#selDataset");
   // Assign the value of the dropdown menu option to a variable
   var dataset = dropdownMenu.property("value");
-  //'names' from importedData?
+  d3.json("data/samples.json").then((importedData) => {
+    var chartData = importedData.samples.filter(sample=> sample.id = id)[0]
+    console.log(chartData)
 
-}
-
-// how to I select dropdown menu and have the 'names' be options to choose in the drop down?
-
-
-//console.log(data)
-
-sample_values = []
-
-otu_ids = []
-
-otu_lables = []
-
-// create compare function to get the top 10 - reverse function?
-
-// // Bar Chart Code
+  // Bar Chart Code
 
   // Create trace.
   var trace1 = {
-    values: sample_values,
-    y: otu_ids,
+    x: chartData.sample_values.slice(0,10),
+    y: chartData.otu_ids.slice(0,10).map(otu => "OTU " + otu),
     orientation: 'h',
-    text: otu_lables
+    text: chartData.otu_labels.slice(0,10),
     type: "bar"
   };
+
 
   // Create the data array for our plot
   var data = [trace1];
 
+  console.log(data)
+
   // Define the plot layout
   var layout = {
-    yaxis: { otu_ids }
+    title: "Top 10 Bacteria Cultures"
   };
 
   // Plot the chart to a div tag with id "bar-plot"
-  Plotly.newPlot("bar-plot", data, layout);
-});
-
+  Plotly.newPlot("bar", data, layout);
 
 
 // // Bubble Chart Code
 // // https://plotly.com/javascript/bubble-charts/
 
-// var trace2 = {
-//     x: otu_ids,
-//     y: sample_values,
-//     mode: 'markers',
-//     marker: {
-//       color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-//       opacity: [1, 0.8, 0.6, 0.4],
-//       size: sample_values
-//     }
-//   };
+var trace2 = {
+    x: chartData.otu_ids,
+    y: chartData.sample_values,
+    mode: 'markers',
+    marker: {
+      color: chartData.otu_ids,
+      size: chartData.sample_values
+    }
+  };
   
-//   var data2 = [trace2];
+  var data2 = [trace2];
   
-//   var layout = {
-//     title: 'Marker Size and Color',
-//     showlegend: false,
-//     height: 600,
-//     width: 600
-//   };
+  var layout2 = {
+    title: 'Bacteria Cultures/Sample',
+    showlegend: false,
+  };
   
-//   Plotly.newPlot('plot', data2, layout);
+  Plotly.newPlot('bubble', data2, layout2);
+
+  var metaData = importedData.metadata.filter(sample=> sample.id = id)[0]
+  console.log(metaData)
+  var demoInfo = d3.select("#sample-metadata");
+  demoInfo.html("")
+  Object.entries(metaData).forEach(([key, value]) => {
+    demoInfo.append('h5').text(key + ": " + value)
+    })
+  })
+}
 
